1d40c4e9b200f41cb092b19ad46e15a6cfdf9199
sunwoojeong <swjeong0825@gmail.com>
2021-08-09 18:03:16 -0400

    add ViolationLogEntity

--- src/main/java/entities/ViolationLogEntity.java
+++ src/main/java/entities/ViolationLogEntity.java
@@ -2,11 +2,52 @@ package entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.time.LocalDateTime;
 
 @Entity
 public class ViolationLogEntity {
     @Id
     private Integer id;
 
+    @ManyToOne
+    private UserEntity user;
 
+    @ManyToOne
+    private ViolationEntity violation;
+
+    private LocalDateTime createdDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public ViolationEntity getViolation() {
+        return violation;
+    }
+
+    public void setViolation(ViolationEntity violation) {
+        this.violation = violation;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
 }
